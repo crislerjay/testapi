b26bf8437ddf66b94bf6d589547d9ad3d0f91ef6
unknown <crislerjayvallo@gmail.com>
2024-06-25 01:51:52 +0800

    fix cors error

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const productRoute = require('./routes/product.route.js')
-require('dotenv').config();
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 // middleware
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
